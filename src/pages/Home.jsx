d12c4e029d4f35abcf8df51717b8a797417709f9
mohamad-taha <mohamadtahakasir@gmail.com>
2026-06-08 01:33:46 +0300

    firebase deploy done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import ProductsSection from "../components/ProductsSection/ProductsSection";
 const Home = () => {
   return (
     <div>
+      <link rel="canonical" href="https://envokem-beauty.web.app/" />
       <meta name="description" content="اكتشف أفضل مواد التنظيف والمعقمات عالية الجودة للمنازل والشركات في ENVOKEM BEAUTY. تشكيلة واسعة، أسعار توفيرية، وتوصيل سريع حتى باب بيتك. تسوق نظافة تدوم الآن!" />
       <Hero />
       <Banner />
